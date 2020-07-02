20a7697361c1a4bec8495a1218e3432942edd662
Shiiisuiii <kolesnik1310.dev@gmail.com>
2020-07-02 20:18:08 +0300

    Fix Style.js task

--- tasks/Styles.js
+++ tasks/Styles.js
@@ -1,7 +1,5 @@
 const {dest, src} = require('../node_modules/gulp');
-
-const {preprocessor} = require('./helpers/VariableHelper.js');
-const {paths} = require('./helpers/PathsHelper');
+const {paths} = require('./helpers/PathsHelper.js');
 
 const sass         = require('gulp-sass');
 const scss         = require('gulp-sass');
@@ -11,16 +9,14 @@ const cleancss     = require('gulp-clean-css');
 const concat       = require('gulp-concat');
 const browserSync  = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
-			  
-let x = paths.src;
-console.log(x);
 
 function styles() {
 	return src(paths.styles.src)
 	.pipe(sass())
+	.pipe(concat(paths.cssOutputName))
 	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
 	.pipe(cleancss( {level: { 1: { specialComments: 0 } },/* format: 'beautify' */ }))
-	.pipe(dest('./dist'))
+	.pipe(dest(paths.styles.dest))
 	.pipe(browserSync.stream())
 }
 
