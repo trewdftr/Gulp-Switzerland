e908ac8177834a4b1f44e46564ba3c5bd75500e6
Shiiisuiii <kolesnik1310.dev@gmail.com>
2020-07-05 23:08:58 +0300

    Add fonts task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 'use strict';
-const { parallel } = require('gulp');
+const { series, parallel } = require('gulp');
 
-const sync     = require('./tasks/Watch');
+const sync        = require('./tasks/Watch');
 const styles      = require('./tasks/Styles');
 const scripts     = require('./tasks/Scripts');
+const fonts       = require('./tasks/Fonts')
 const images      = require('./tasks/Images');
-const startWatch    = require('./tasks/Watch')
+const startWatch  = require('./tasks/Watch')
 
-exports.start = sync.server 
+exports.start   = sync.server 
 exports.styles  = styles.styles;
 exports.scripts = scripts.scripts;
 exports.images  = images.images;
+exports.fonts = fonts.toWoff2;
 exports.startWatch  = startWatch.startWatch;
-exports.build   = parallel(images.images, styles.styles, scripts.scripts, sync.server, startWatch.startWatch);
+exports.build   = parallel(series(images.images, fonts.toWoff2, styles.styles, scripts.scripts), sync.server, startWatch.startWatch);
