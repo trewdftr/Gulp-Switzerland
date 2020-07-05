a93766407875bb78390acc68a530bfce782c5e43
Shiiisuiii <kolesnik1310.dev@gmail.com>
2020-07-05 23:17:45 +0300

    Add font task

---
+++ tasks/Fonts.js
@@ -0,0 +1,13 @@
+const { src, dest } = require('gulp');
+const { paths } = require('./helpers/PathsHelper');
+const converter = require('gulp-ttf2woff2');
+const changed = require('gulp-changed');
+
+function toWoff2() {
+    return src(paths.fonts.src)
+    .pipe(changed(paths.fonts.dest, {extension: '.woff2'}))
+    .pipe(converter())
+    .pipe(dest(paths.fonts.dest))
+}
+
+exports.toWoff2 = toWoff2;
