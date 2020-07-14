b29c8ef3d817fe5fcc0e2cf4cda03f6a81fc7f33
Shiiisuiii <kolesnik1310.dev@gmail.com>
2020-07-14 18:26:41 +0300

    Remove Fn to String function

--- tasks/Styles.js
+++ tasks/Styles.js
@@ -10,21 +10,10 @@ const cleancss     = require('gulp-clean-css');
 const concat       = require('gulp-concat');
 const browserSync  = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
-const { stream } = require('browser-sync');
-
-
-global.runFunctionByName = function runFunctionByName(functionName, context = global) {
- 	return context[functionName];
-}
-
-stream
-
-const x = runFunctionByName(preprocessor);
-console.log(x);
 
 function styles() {
 	return src(paths.styles.src)
-	.pipe(runFunctionByName(preprocessor).on('error', preprocessor.logError))
+	.pipe(stylus())
 	.pipe(concat(paths.cssOutputName))
 	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
 	.pipe(cleancss( {level: { 1: { specialComments: 0 } }, format: 'beautify'  }))
