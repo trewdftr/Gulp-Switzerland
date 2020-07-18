4c64d998a4c8e9fee1005cfae886e97aba058ead
Shiiisuiii <kolesnik1310.dev@gmail.com>
2020-07-18 21:06:01 +0300

    Delete unusable exports

--- tasks/helpers/VariableHelper.js
+++ tasks/helpers/VariableHelper.js
@@ -1,10 +1,5 @@
-
 let baseDir  = 'src'; // If you wanna change you main dev directory, just replace src on another name
 preprocessor = 'stylus', // If you work with less or scss, just change 'sass' on 'less' for example.
-fileswatch   = 'html,htm,txt,json,md,woff2', // List of files extensions for watching & hard reload (comma separated)
-imageswatch  = 'jpg,jpeg,png,webp,svg'; // List of images extensions for watching & compression (comma separated)
 
 exports.baseDir      = baseDir;
 exports.preprocessor = preprocessor;
-exports.fileswatch   = fileswatch;
-exports.imageswatch  = imageswatch;
