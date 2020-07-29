7e08751823d01d84ea910bd62db56fb469f59f5a
Savauge <kolesnik1310.dev@gmail.com>
2020-07-29 09:56:15 +0300

    Add correct html and images paths

--- tasks/helpers/VariableHelper.js
+++ tasks/helpers/VariableHelper.js
@@ -1,5 +1,11 @@
-let baseDir  = 'src'; // If you wanna change you main dev directory, just replace src on another name
-preprocessor = 'stylus', // If you work with less or scss, just change 'sass' on 'less' for example.
+let baseDir                                         = 'src'; // If you wanna change you main dev directory, just replace src on another name
+let preprocessor = 'scss'; // If you work with less either sass, stylus, just change 'scss' on 'less' for example.
+let html = '*.html';
+let imagesWatch = '*.jpeg, *.jpg, *.webp, *.png, *.svg'
+
+
 
 exports.baseDir      = baseDir;
 exports.preprocessor = preprocessor;
+exports.html         = html;
+exports.imagesWatch  = imagesWatch;
