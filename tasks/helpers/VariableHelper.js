da8e250e36c33c9c9bfbbc8c2347a79a4ac473fc
Savauge <kolesnik1310.dev@gmail.com>
2020-07-31 17:11:26 +0300

    Add deploy path for boilerplate

--- tasks/helpers/VariableHelper.js
+++ tasks/helpers/VariableHelper.js
@@ -1,11 +1,13 @@
-let baseDir                                         = 'src'; // If you wanna change you main dev directory, just replace src on another name
+let baseDir = 'src'; // If you wanna change you main dev directory, just replace src on another name
+let deployDir = 'public'
 let preprocessor = 'scss'; // If you work with less either sass, stylus, just change 'scss' on 'less' for example.
 let html = '*.html';
 let imagesWatch = '*.jpeg, *.jpg, *.webp, *.png, *.svg'
 
 
 
-exports.baseDir      = baseDir;
+exports.baseDir = baseDir;
+exports.deployDir = deployDir
 exports.preprocessor = preprocessor;
 exports.html         = html;
 exports.imagesWatch  = imagesWatch;
