0f6ff61b6adeb636af1cdd97d37437b54103f348
Savauge <kolesnik1310.dev@gmail.com>
2020-07-31 17:03:44 +0300

    Add build task for boilerplate

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
 'use strict';
 const { series, parallel } = require('gulp');
 
-const watch        = require('./tasks/Watch');
+const watch       = require('./tasks/Watch');
 const styles      = require('./tasks/Styles');
 const scripts     = require('./tasks/Scripts');
 const fonts       = require('./tasks/Fonts');
 const images      = require('./tasks/Images');
+const html        = require('./tasks/Html');
+const assets      = require('./tasks/Public');
 
-exports.start   = watch.server 
+exports.start = watch.server;
+exports.html = html.html;
 exports.styles  = styles.styles;
 exports.scripts = scripts.scripts;
 exports.images  = images.images;
+exports.convertFonts = series(fonts.toWoff2, fonts.toWoff, fonts.toSvg, fonts.toEot, fonts.ttfRebase)
 exports.startWatch = watch.startWatch;
-exports.build   = parallel(series(images.images, fonts.toWoff2, fonts.ttfRebase ,styles.styles, scripts.scripts), watch.server, watch.startWatch);
+exports.dev   = series(series(this.convertFonts, images.images, styles.styles,scripts.scripts), 
+parallel(watch.server, watch.startWatch));
+exports.build = series(assets.publicFonts, assets.publicImages, parallel(assets.publicCss, assets.publicJs, html.html));
+
 
