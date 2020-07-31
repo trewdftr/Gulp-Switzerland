d9e7609ca9f9963e70b606fd5c487ff34ee3d0cf
Savauge <kolesnik1310.dev@gmail.com>
2020-07-31 17:10:26 +0300

    Converter functions for Fonts task

--- tasks/Fonts.js
+++ tasks/Fonts.js
@@ -1,12 +1,36 @@
 const { src, dest } = require('gulp');
 const { paths } = require('./helpers/PathsHelper');
-const converter = require('gulp-ttf2woff2');
+const ttf2ToWoff2 = require('gulp-ttf2woff2');
+const ttf2ToWoff = require('gulp-ttf2woff');
+const ttf2ToSvg = require('gulp-ttf-svg');
+const ttf2Eot = require('gulp-ttf2eot');
 const changed = require('gulp-changed');
 
 function toWoff2() {
     return src(paths.fonts.src)
     .pipe(changed(paths.fonts.dest, {extension: '.woff2'}))
-    .pipe(converter())
+    .pipe(ttf2ToWoff2())
+    .pipe(dest(paths.fonts.dest))
+}
+
+function toWoff() {
+    return src(paths.fonts.src)
+    .pipe(changed(paths.fonts.dest, {extension: '.woff'}))
+    .pipe(ttf2ToWoff())
+    .pipe(dest(paths.fonts.dest))
+}
+
+function toSvg() {
+    return src(paths.fonts.src)
+    .pipe(changed(paths.fonts.dest, {extension: '.svg'}))
+    .pipe(ttf2ToSvg())
+    .pipe(dest(paths.fonts.dest))
+}
+
+function toEot() {
+    return src(paths.fonts.src)
+    .pipe(changed(paths.fonts.dest, {extension: '.eot'}))
+    .pipe(ttf2Eot())
     .pipe(dest(paths.fonts.dest))
 }
 
@@ -15,5 +39,9 @@ function ttfRebase() {
     .pipe(dest(paths.fonts.dest))
 }
 
+exports.toWoff2 = toWoff2;
+exports.toWoff = toWoff;
+exports.toSvg = toSvg;
+exports.toEot = toEot;
 exports.toWoff2 = toWoff2;
 exports.ttfRebase = ttfRebase;
