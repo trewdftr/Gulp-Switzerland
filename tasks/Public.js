e09a427662d0d3a847a0ce2108c30e866f59b1b5
Savauge <kolesnik1310.dev@gmail.com>
2020-08-01 22:45:14 +0300

    Task for bundling everything and ready to be use in production

---
+++ tasks/Public.js
@@ -0,0 +1,30 @@
+const { src, dest } = require('gulp');
+const { paths } = require('./helpers/PathsHelper');
+let { deployDir } = require('./helpers/VariableHelper'); 
+
+function publicCss () {
+    return src(paths.styles.srcPublic)
+    .pipe(dest(paths.styles.public))    
+}
+
+function publicJs () {
+    return src(paths.scripts.srcPublic)
+    .pipe(dest(paths.scripts.public))    
+}
+
+
+function publicFonts () {
+    return src(paths.fonts.srcPublic)
+    .pipe(dest(paths.fonts.public))    
+}
+
+function publicImages () {
+    return src(paths.images.srcPublic)
+    .pipe(dest(paths.images.public))    
+}
+
+
+exports.publicCss = publicCss;
+exports.publicJs = publicJs;
+exports.publicFonts = publicFonts;
+exports.publicImages= publicImages;
