718339528f5a69b52f61d03860a79c0ff93e3fea
Savauge <kolesnik1310.dev@gmail.com>
2020-08-01 22:43:22 +0300

    Add cache for images

--- tasks/Images.js
+++ tasks/Images.js
@@ -2,11 +2,12 @@ const { src, dest } = require('gulp');
 const changed     = require('gulp-changed');
 const imagemin    = require('gulp-imagemin');
 const { paths }   = require('./helpers/PathsHelper');
+const cache = require('gulp-cache')
 
 function images () {
     return src(paths.images.src)
     .pipe(changed(paths.images.dest))
-    .pipe(imagemin())
+    .pipe(cache(imagemin()))
     .pipe(dest(paths.images.dest))
 }
 
