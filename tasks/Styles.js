f4c6e6fd72834e6439faa545e9ac98b4972c2de7
Savauge <kolesnik1310.dev@gmail.com>
2020-08-01 22:46:51 +0300

    Add postcss with uncss for checking unusable css

--- tasks/Styles.js
+++ tasks/Styles.js
@@ -8,17 +8,38 @@ const stylus       = require('gulp-stylus');
 const cleancss     = require('gulp-clean-css');
 const concat       = require('gulp-concat');
 const browserSync  = require('browser-sync').create();
-const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+const sourcemap = require('gulp-sourcemaps');
+const uncss = require('postcss-uncss');
 
 
 function styles() {
-	return src(paths.styles.src)
-	.pipe(stylus())
+	return src([paths.styles.src, paths.styles.dest])
+	.pipe(sass())
+	.pipe(sourcemap.init())
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } }, format: 'beautify' }))
 	.pipe(concat(paths.cssOutputName))
-	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
-	.pipe(cleancss( {level: { 1: { specialComments: 0 } }, format: 'beautify'  }))
+	.pipe(sourcemap.write('.'))
 	.pipe(dest(paths.styles.dest))
 	.pipe(browserSync.stream())
 }
 
+
+function unusable () {
+	
+	let plugins = [
+		uncss({
+			html: ['src/index.html']
+		})
+	];
+
+	
+	return src(paths.styles.dest + '/app.min.css') 
+	.pipe(postcss(plugins))
+	.pipe(dest('src/css/'))
+}
+
+
 exports.styles = styles;
+exports.unusable = unusable;
