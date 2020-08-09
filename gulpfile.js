0f962982b09d548de62e12deb5859aef8b3b5bfc
Savauge <kolesnik1310.dev@gmail.com>
2020-08-09 20:07:03 +0300

    Remove replace task

--- gulpfile.js
+++ gulpfile.js
@@ -10,19 +10,49 @@ const html        = require('./tasks/Html');
 const assets      = require('./tasks/Public');
 const unusable    = require('./tasks/Styles');
 const clean       = require('./tasks/Clean');
-  
 
-exports.convertImages = series(images.images,  images.resizeSm, images.resizeMd, images.resizeLg,images.resizeSm2x,images.resizeMd2x,images.resizeLg2x,
-images.resizeSm3x,images.resizeMd3x,images.resizeLg3x, images.cachemin);
+exports.convertImages = 
+    series
+    (
+        images.images,
+        images.resizeSm, 
+        images.resizeMd, 
+        images.resizeLg,
+        images.resizeSm2x,
+        images.resizeMd2x,
+        images.resizeLg2x,
+        images.resizeSm3x,
+        images.resizeMd3x,
+        images.resizeLg3x, 
+        images.cachemin
+    );
 
-exports.convertFonts = series(fonts.toWoff2, fonts.toWoff, fonts.toEot, fonts.ttfRebase);
+exports.convertFonts = 
+    series
+    (
+        fonts.toWoff2, 
+        fonts.toWoff, 
+        fonts.toEot, 
+        fonts.ttfRebase
+    );
 
-exports.dev = series(series(this.convertFonts, this.convertImages, styles.styles, scripts.scripts), 
-parallel(watch.server, watch.startWatch));
+exports.dev = series(
+    series
+    (
+        this.convertFonts, 
+        this.convertImages, styles.styles, 
+        scripts.scripts,
+    ), 
+    parallel(watch.server));
 
 exports.devClear = clean.cleanSrc;
 
-exports.build = series(clean.cleanPublic, unusable.unusable, assets.publicFonts, assets.publicImages, 
-parallel(assets.publicCss, assets.publicJs, html.html));
-
-
+exports.build = series(
+    series
+    (
+        clean.cleanPublic,
+        unusable.unusable, 
+        assets.publicFonts, 
+        assets.publicImages
+    ), 
+    parallel(assets.publicCss, assets.publicJs, html.html));
