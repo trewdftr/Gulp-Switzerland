7f104af4fb2d263d89b6448af35129b1c4a0ccf7
Savauge <kolesnik1310.dev@gmail.com>
2020-08-09 20:14:16 +0300

    Set default preproccessor - SCSS

--- tasks/helpers/VariableHelper.js
+++ tasks/helpers/VariableHelper.js
@@ -1,13 +1,13 @@
-let baseDir = 'src'; // If you wanna change you main dev directory, just replace src on another name
+let baseDir = 'src'; // If you wanna change you main dev directory, just replace src to another name
 let deployDir = 'public'
-let preprocessor = 'scss'; // If you work with less either sass, stylus, just change 'scss' on 'less' for example.
+let preprocessor = 'scss'; // If you work with less either sass, stylus, just change 'scss' to 'less' for example.
 let html = '*.html';
 let imagesWatch = '*.jpeg, *.jpg, *.webp, *.png, *.svg'
 
 
 
-exports.baseDir = baseDir;
-exports.deployDir = deployDir
+exports.baseDir      = baseDir;
+exports.deployDir    = deployDir
 exports.preprocessor = preprocessor;
 exports.html         = html;
 exports.imagesWatch  = imagesWatch;
