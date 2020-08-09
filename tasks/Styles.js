a142bf5494dde5b90f3c1a7b8341b0770a3821e1
Savauge <kolesnik1310.dev@gmail.com>
2020-08-09 20:15:21 +0300

    Replace uncss to purge, uncss has a error https://github.com/uncss/uncss/issues/430

--- tasks/Styles.js
+++ tasks/Styles.js
@@ -11,34 +11,25 @@ const browserSync  = require('browser-sync').create();
 const autoprefixer = require('autoprefixer');
 const postcss = require('gulp-postcss');
 const sourcemap = require('gulp-sourcemaps');
-const uncss = require('postcss-uncss');
-
+const purge = require('@fullhuman/postcss-purgecss');
 
 function styles() {
 	return src(paths.styles.src)
 	.pipe(sourcemap.init())
-	.pipe(sass())
+	.pipe(scss())
+	.pipe(postcss([autoprefixer()]))
 	.pipe(cleancss( {level: { 1: { specialComments: 0 } }, format: 'beautify' }))
 	.pipe(concat(paths.cssOutputName))
-	.pipe(sourcemap.write('./map'))
+	.pipe(sourcemap.write('.'))
 	.pipe(dest(paths.styles.dest))
 	.pipe(browserSync.stream())
 }
 
-
 function unusable () {
-	
-	let plugins = [
-		uncss({
-			html: ['src/index.html']
-		})
-	];
-	
 	return src(paths.styles.dest + '/app.min.css') 
-	.pipe(postcss(plugins))
+	.pipe(postcss([purge({content: ['src/index.html']})]))
 	.pipe(dest('src/css/'))
 }
-
 
 exports.styles = styles;
 exports.unusable = unusable;
